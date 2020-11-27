aa175ebe9c528e162c1a2559d3d46289ecf4de90
SSGSuperstitch <73788362+SSGSuperstitch@users.noreply.github.com>
2020-11-27 17:34:51 -0500

    All Code

---
+++ index.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const {
+    token,
+    prefix,
+} = require('./config.json')
+const fs = require('fs')
+const client = new Discord.Client()
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command)
+}
+
+client.once('ready', () => {
+    console.log(`Logged om as ${client.user.tag}`)
+});
+
+
+client.on("message", message =>{
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+  if (command === 'ping'){
+      client.commands.get('ping').execute(message, args)
+  }
+
+
+})
+
+
+
+
+
+
+client.login(token)
